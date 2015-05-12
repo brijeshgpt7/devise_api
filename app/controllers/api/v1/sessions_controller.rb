82fa754521c83463bce6ef616f6d53088d454834
Brijesh Kumar Gupta <brijesh@carmatec.com>
2015-05-12 11:19:40 +0530

    curl command

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -24,16 +24,26 @@ class Api::V1::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
-    warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+    debugger
+    if User.find_by_email(params[:user][:email]).present?
+    warden.authenticate!(:scope => resource_name)
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in" }
                       #,:data => { :auth_token => current_user.authentication_token }
+    else 
+       render :status => 401,
+           :json => { :success => false,
+                      :info => "Login Failed",
+                      :data => {} }               
+                      
+    end
   end
 
   def destroy
+    debugger
     warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-    current_user.update_column(:authentication_token, nil)
+    #current_user.update_column(:authentication_token, nil)
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged out",
